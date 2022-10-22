e942d2b82b1406a894f270e4f0632aa38be2e352
Espe Cabrer <mecabrerv@gmail.com>
2022-10-22 17:54:00 +0200

    Tests: Third party

--- src/main/java/com/ironhack/banksystem/thirdParty/ThirdParty.java
+++ src/main/java/com/ironhack/banksystem/thirdParty/ThirdParty.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ThirdParty {
+public class ThirdPartyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,7 +23,7 @@ public class ThirdParty {
     @Column(unique = true)
     private String name;
 
-    public ThirdParty(String hashedKey, String name) {
+    public ThirdPartyEntity(String hashedKey, String name) {
         this.hashedKey = hashedKey;
         this.name = name;
     }
---
+++ src/test/java/com/ironhack/banksystem/thirdParty/ThirdPartyServiceTests.java
@@ -0,0 +1,2 @@
+package com.ironhack.banksystem.thirdParty;public class ThirdPartyServiceTests {
+}
